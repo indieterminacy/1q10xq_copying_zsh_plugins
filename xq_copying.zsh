c10c36dc3c21deea45784dcdd86bc52d548ac84c
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-23 16:14:47 +0200

    Starting plugins config for copying tools. None are active atm

---
+++ xq_copying.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/copyfile
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/copydir
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/cp
